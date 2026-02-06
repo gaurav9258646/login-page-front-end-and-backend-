b33c5ff51cb8c8d03c5dd6704b4df447f3bf6141
Gaurav <gauravsaini.pcm@gmail.com>
2026-02-06 12:01:47 +0530

    feat: add MongoDB connection setup🔨

---
+++ Login-page-backend/src/db/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+  async function connectDB() {
+    try{
+        await mongoose.connect(process.env.MONGODB_URL);
+           console.log('MongoDB connected successfully');
+
+    }catch(error){
+        console.log('MongoDB connection failed:', error.message);
+        process.exit(1);
+
+    }
+    
+  }
+
+  module.exports = connectDB;
